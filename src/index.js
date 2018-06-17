a872542c610868e16c5b8e2621c528084349e8ed
yuning <djyuning@126.com>
2018-06-17 18:46:55 +0800

    使用 Kube 主题模板；优化静态资源调用；编辑器采用 2 个空格缩进；

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
 const path = require('path');
 const Koa = require('koa');
 const route = require('koa-route');
+const static = require('koa-static');
 const render = require('koa-ejs');
 
 const app = new Koa();
 
 render(app, {
-  root: path.join(__dirname, 'view'),
-  viewExt: 'html',
-  cache: false,
-  debug: true
+	root: path.join(__dirname, 'view'),
+	layout: 'Layout/main',
+	viewExt: 'html',
+	cache: false,
+	debug: false,
 });
 
-// 首页
-app.use(route.get('/', async ctx => {
-  ctx.body = 'Hello World';
+// 处理静态资源
+app.use(static(path.join(__dirname, '/static')));
 
-  await ctx.render('index');
+// 首页
+app.use(route.all('/', async ctx => {
+	await ctx.render('Index/index');
 }));
 
 // 404
 app.use(route.all('*', async ctx => {
-  ctx.response.status = 404;
-  ctx.response.body = '<h1>Page Not Found</h1>';
-  await ctx.render('404');
+	ctx.response.status = 404;
+	ctx.response.body = '<h1>Page Not Found</h1>';
+	await ctx.render('404');
 }));
 
-app.listen(3000);
+app.listen(3000, () => {
+	console.log('server is starting at port 3000');
+});
